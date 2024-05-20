0cf9841e68c5b333ddeab36d3a871f772c051346
LuisPuente13 <88508603+LuisPuente13@users.noreply.github.com>
2024-05-19 18:57:06 -0600

    Modificaciones en interfaz Inicio

--- Frontend/pages/PaginaInicioAdmin/Inicio.js
+++ Frontend/pages/PaginaInicioAdmin/Inicio.js
@@ -21,13 +21,20 @@ document.addEventListener("DOMContentLoaded", function() {
     saveBtn.onclick = function() {
         var title = document.getElementById("editTitle").value;
         var description = document.getElementById("editDescription").value;
-        var image = document.getElementById("editImage").value;
+        var mainImage = document.getElementById("editMainImage").value;
+        var section1Image = document.getElementById("editSection1Image").value;
+        var section1Text = document.getElementById("editSection1Text").value;
+        var section2Image = document.getElementById("editSection2Image").value;
+        var section2Text = document.getElementById("editSection2Text").value;
 
         document.querySelector(".title").textContent = title;
         document.querySelector(".description").textContent = description;
-        document.querySelector(".background-image").style.backgroundImage = `url(${image})`;
+        document.querySelector(".background-image").style.backgroundImage = `url(${mainImage})`;
+        document.querySelectorAll(".section-image img")[0].src = section1Image;
+        document.querySelectorAll(".section-text")[0].textContent = section1Text;
+        document.querySelectorAll(".section-image img")[1].src = section2Image;
+        document.querySelectorAll(".section-text")[1].innerHTML = section2Text;
 
         modal.style.display = "none";
     }
 });
-
