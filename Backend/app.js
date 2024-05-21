009f492cb5355eeab5f573eaf027f8f8034ac2df
Elizabeth17-7 <eli.amalia177@gmail.com>
2024-05-20 22:41:11 -0600

    acciones de apis

---
+++ Backend/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import authRoute from "./apis/auth.js";
+import catalogsRoute from "./apis/catalogs.js";
+import disastersRoute from "./apis/disasters.js";
+import packages from "./apis/packages.js";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use("/api/auth", authRoute);
+app.use("/api/catalogs", catalogsRoute);
+app.use("/api/disasters", disastersRoute);
+app.use("/api/packages", packages);
+
+app.listen(port, () => {
+  console.log(`Helptous apis listening on port ${port}`);
+});
